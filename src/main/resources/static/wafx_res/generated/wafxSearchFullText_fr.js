55f7c5a6d3027a37b777d4af39847312599cb9f0
Helton Campos <heltonscamopos@gmail.com>
2021-04-01 15:05:12 -0300

    site com menu sem CPF

--- src/main/resources/static/wafx_res/generated/wafxSearchFullText_fr.js
+++ src/main/resources/static/wafx_res/generated/wafxSearchFullText_fr.js
@@ -4,7 +4,7 @@
  , {  "title":"Page sans titre" , "display_url":"index.html" , "url":"index.html" } 
  , {  "title":"Page sans titre" , "display_url":"f8f48431_00f1_4481_80bc_61b309380e57.html" , "url":"f8f48431_00f1_4481_80bc_61b309380e57.html" } 
 ],
-"map_words":["","10","3","accenture","adaptativa","administracao","administrador","agilidade","ambientes","americano","analise","anos","anteriormente","aqui","artes","atualmente","automacao","bacharel","brasileiro","cfengine","ciencia","ciencias","coisas","com","como","computacao","concrete","configurando","cpf","cultura","curso","da","de","dentro","departamento","desenvolvemos","desenvolvimento","devops","dicas","digitais","do","docker","e","em","empresa","engenharia","engenheiro","entregando","especialista","estagiario","estar","esteja","estrutura","excelentes","experiencia","fazendo","fui","fundamentals","graduando","home","imd","informatica","infraestrutura","inovacao","integrado","intelbras","latino","linux","mercado","monitoracao","move","movemos","mundo","na","nao","no","nos","nossa","o","para","parceria","pela","poderia","por","possui","praticas","pre","producao","produtos","programadoras","programadores","projeto","provisionar","puppet","que","queo","redes","residente","rn","sass","senior","servicos","servidores","sete","setor","sistemas","sobre","solucoes","sou","superintendencia","suporte","sysadmin","talvez","tambem","tecnico","tecnologo","telecomunicacoes","tenho","tenologia","ti","time","trabalhei","tre","tudo","ufrn","uma","utilizando","validar","voce","windows","wisefi","xencenter"]
+"map_words":["","10","3","accenture","adaptativa","administracao","administrador","agilidade","ambientes","americano","analise","anos","anteriormente","aqui","artes","atualmente","automacao","bacharel","brasileiro","cfengine","ciencia","ciencias","coisas","com","como","computacao","concrete","configurando","cultura","curso","da","de","dentro","departamento","desenvolvemos","desenvolvimento","devops","dicas","digitais","do","docker","e","em","empresa","engenharia","engenheiro","entregando","especialista","estagiario","estar","esteja","estrutura","excelentes","experiencia","fazendo","fui","fundamentals","graduando","home","imd","informatica","infraestrutura","inovacao","integrado","intelbras","latino","linux","mercado","monitoracao","move","movemos","mundo","na","nao","no","nos","nossa","o","para","parceria","pela","poderia","por","possui","praticas","pre","producao","produtos","programadoras","programadores","projeto","provisionar","puppet","que","queo","redes","residente","rn","sass","senior","servicos","servidores","sete","setor","sistemas","sobre","solucoes","sou","superintendencia","suporte","sysadmin","talvez","tambem","tecnico","tecnologo","telecomunicacoes","tenho","tenologia","ti","time","trabalhei","tre","tudo","ufrn","uma","utilizando","voce","windows","wisefi","xencenter"]
 ,
 "index_words":[ [0,1,2 ] 
 , [1 ] 
@@ -34,7 +34,6 @@
 , [2 ] 
 , [0,1,2 ] 
 , [2 ] 
-, [0,1,2 ] 
 , [0,2 ] 
 , [2 ] 
 , [0,2 ] 
@@ -133,7 +132,6 @@
 , [2 ] 
 , [2 ] 
 , [2 ] 
-, [0,1,2 ] 
 , [1 ] 
 , [2 ] 
 , [2 ] 
